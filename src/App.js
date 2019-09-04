3939d9c20fc9496c35c7beeb386f5763c889600d
Borrom Dev <borrom.dev@gmail.com>
2019-09-04 21:38:56 +0700

    ch-05: add csm content

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import  HomePage from "./home/HomePage";
 import { Switch, Route} from "react-router-dom";
 import LoginPage from './login/LoginPage';
 import PrivateRoute from './component/PrivateRoute';
+import RegisterForm from './register/RegisterPage';
 const App =() => (
     <Switch>
+      <Route path="/register" component={RegisterForm}/>
       <Route path="/login" component={LoginPage}/>
       <PrivateRoute path="/" exact component={HomePage}/>
     </Switch>
