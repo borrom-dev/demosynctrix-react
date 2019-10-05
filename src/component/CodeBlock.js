c5c702abc126d0dca5887ea612b92ea4815e5754
Borrom Sync <borrom.dev@gmail.com>
2019-10-05 22:29:16 +0700

    ch-21: add inline code

--- src/component/CodeBlock.js
+++ src/component/CodeBlock.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 class CodeBlock extends PureComponent {
   static propTypes = {
@@ -16,7 +16,7 @@ class CodeBlock extends PureComponent {
   render() {
     const { language, value } = this.props;
     return (
-      <SyntaxHighlighter language={language} style={okaidia}>
+      <SyntaxHighlighter useInlineStyles language={language} style={darcula}>
         {value}
       </SyntaxHighlighter>
     );
